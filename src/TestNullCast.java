758f8dc9bd7be1ce207602832fea16dac0d25253
santosh.k.k <santhosh.kk@verse.in>
2017-09-06 15:29:33 +0530

    more

---
+++ src/TestNullCast.java
@@ -0,0 +1,9 @@
+
+public class TestNullCast {
+
+  public static void main(String[] args){
+
+  Integer i = (Integer)null;
+  System.out.println("Hello");
+  }
+}
--- src/algo/graph/ShortestPathWeightedGraph.java
+++ src/algo/graph/ShortestPathWeightedGraph.java
@@ -7,7 +7,7 @@ import java.util.List;
 import java.util.Scanner;
 import java.util.Set;
 
-public class ShortestPathWrightedGraph {
+public class ShortestPathWeightedGraph {
 
 	public static void main(String[] args) {
 		Scanner s = new Scanner(System.in);
