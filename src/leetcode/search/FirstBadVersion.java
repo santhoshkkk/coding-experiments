5e0892beea7d2a6295f89545934b1ce8f7c6183f
santhoshkkk <sannthoshkumar.krishnakurup@gmail.com>
2020-12-30 23:49:08 +0530

    first bad version - improved submission 2

--- src/leetcode/search/FirstBadVersion.java
+++ src/leetcode/search/FirstBadVersion.java
@@ -2,13 +2,12 @@ package leetcode.search;
 
 /**
  * You are a product manager and currently leading a team to develop a new product. Unfortunately, the latest version of your product fails the quality check. Since each version is developed based on the previous version, all the versions after a bad version are also bad.
- *
+ * <p>
  * Suppose you have n versions [1, 2, ..., n] and you want to find out the first bad one, which causes all the following ones to be bad.
- *
+ * <p>
  * You are given an API bool isBadVersion(version) which returns whether version is bad. Implement a function to find the first bad version. You should minimize the number of calls to the API.
  */
 public class FirstBadVersion {
-
     private final int badVersion;
 
     public FirstBadVersion(int badVersion) {
@@ -27,29 +26,22 @@ public class FirstBadVersion {
 
     public int firstBadVersion(int n) {
 
-        return firstBadVersion(0, n);
-    }
-
-    private int firstBadVersion(int start, int end) {
-
+        int start = 0;
+        int end = n;
+        int middle = -1;
         int badVersion = -1;
-        int smallerBadVersion = -1;
 
-        int middle = (start + end) / 2;
-        if (isBadVersion(middle)) {
-            badVersion = middle;
-            if (start != middle) {
-                smallerBadVersion = firstBadVersion(start, middle);
-                if (smallerBadVersion != -1) {
-                    badVersion = smallerBadVersion;
-                }
+        while (start <= end && middle != start) {
+            long sum = (long) start + end;
+            middle = (int) (sum / 2);
+            if (isBadVersion(middle)) {
+                badVersion = middle;
+                end = middle;
+            } else {
+                start = middle + 1;
             }
-        } else {
-            if (middle + 1 <= end) {
-                badVersion = firstBadVersion(middle + 1, end);
-            }
-
         }
+
         return badVersion;
     }
 }
