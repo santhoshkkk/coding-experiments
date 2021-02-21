556d9e194195e53267549a9706cae21a82b5dd2a
santhoshkkk <santhoshkumar.krishnakurup@gmail.com>
2021-02-21 21:52:20 +0530

    Rotate array algorithm2 memory efficient

--- src/leetcode/array/RoatateArray.java
+++ src/leetcode/array/RoatateArray.java
@@ -4,18 +4,24 @@ public class RoatateArray {
     public static void main(String[] args) {
         int[] nums = {1, 2, 3};
         rotate(nums, 2);
-        for(int i=0;i<nums.length; i++){
+        for (int i = 0; i < nums.length; i++) {
             System.out.print(nums[i] + " ");
         }
     }
 
     public static void rotate(int[] nums, int k) {
-        int temp[] = new int[nums.length];
-        for (int i = 0; i < nums.length; i++) {
-            temp[(i + k) % nums.length] = nums[i];
-        }
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = temp[i];
+        int next;
+        int temp;
+        int nextIndex;
+        k=k%nums.length;
+        for (int i = 0; i < k; i++) {
+            next = nums[0];
+            for (int j = 0; j < nums.length; j++) {
+                nextIndex=(j + 1) % nums.length;
+                temp = nums[nextIndex];
+                nums[nextIndex] = next;
+                next = temp;
+            }
         }
     }
 }
